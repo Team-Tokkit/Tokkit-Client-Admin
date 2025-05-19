a241eb25c7678fc593e6481809d2499546adb119
조윤주 <155442976+iamyuunzo@users.noreply.github.com>
2025-05-19 10:40:35 +0900

    Refactor: 관리자 페이지 컴포넌트 분리 (#4)
    * feat: 공통 MainLayout 및 Header/Sidebar 컴포넌트 생성
    
    * refactor: 공통 Layout 분리 및 page.tsx 리다이렉트 경로 수정
    
    * chore: 기능별 폴더 구조 생성 및 page.tsx 템플릿 추가
    
    * refactor: login 화면 페이지 기본 진입 경로 설정

---
+++ app/auth/page.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function UserPage() {
+  return <div>인증 관리 페이지입니다.</div>
+}
---
+++ app/logs/page.tsx
@@ -0,0 +1,5 @@
+'use client'
+
+export default function UserPage() {
+  return <div>시스템 로그 페이지입니다.</div>
+}
