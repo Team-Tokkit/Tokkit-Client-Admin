f00581b32daf078e21de9d96470ef1333878acd4
amy8883 <amy8883@naver.com>
2025-06-04 17:05:01 +0900

    :refactor: refactor: Prefix admin-api로 변경

--- app/login/api/authApi.ts
+++ app/login/api/authApi.ts
@@ -2,12 +2,15 @@ import apiClient from "@/lib/apiClient";
 
 // 🔐 로그인 요청
 export async function loginAdmin(email: string, password: string) {
-  const response = await apiClient.post("/admin/login", { email, password });
+  const response = await apiClient.post("/admin-api/login", {
+    email,
+    password,
+  });
   return response.data;
 }
 
 // 🔓 로그아웃 요청
 export async function logoutAdmin() {
-  const response = await apiClient.post("/admin/logout");
+  const response = await apiClient.post("/admin-api/logout");
   return response.data;
 }
